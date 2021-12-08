e5de9ff841cea4660aa2bc7e38cc9c8ca238a54b
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-08 23:38:32 +0700

    Using a Helper Function for Navigation

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const adminRoutes = require('./routes/admin');
+const bodyParser = require('body-parser');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res ,next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //Khong can muc nua
+});
+app.listen(3000);

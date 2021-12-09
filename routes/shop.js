c5312ba180bae8c857aa6290186088de9a4288d4
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 13:10:59 +0700

    install and implement pug to views/.pug

---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+const path = require('path');
+
+const rootDir  = require('../util/path');
+
+const adminData  = require('./admin');
+
+router.get('/', (req, res , next) => {
+   // res.sendFile(path.join(rootDir, 'views' , 'shop.html'));
+   // console.log('shop.js', adminData.products);
+   res.render('shop');
+});
+module.exports = router ;

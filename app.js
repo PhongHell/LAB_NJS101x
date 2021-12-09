ca61aaa40453444dbcef561cf481a4a88cdf99ca
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 13:24:17 +0700

    finish pug

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+const adminData = require('./routes/admin');
+const bodyParser = require('body-parser');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug');//set pug
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+app.use((req, res ,next) =>{
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404' , {pageTitle : "Page Not Found"});
+});
+app.listen(3000);

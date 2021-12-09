01b7b439fa230c1c663a770c1082b4e1e4de2049
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 13:08:53 +0700

    sharing data across data requests , user to admin.js/routes

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+const adminData = require('./routes/admin');
+const bodyParser = require('body-parser');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+app.use((req, res ,next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //Khong can muc nua
+});
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const e = require('express');
+const express = require('express');
+const router  = express.Router();
+const path = require('path');
+
+const rootDir  = require('../util/path');
+
+const adminData  = require('./admin');
+
+router.get('/', (req, res , next) => {
+   res.sendFile(path.join(rootDir, 'views' , 'shop.html'));
+   console.log('shop.js', adminData.products);
+});
+module.exports = router ;

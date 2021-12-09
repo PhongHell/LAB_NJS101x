f69cc973f9e6fb7724a3242daa5997802086429e
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 22:30:43 +0700

    fetch data model/product.js

---
+++ controllers/admin.js
@@ -0,0 +1,67 @@
+const Products = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    docTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const products = new Products(false, title, imageUrl, description, price);
+  products.save();
+  res.redirect("/products");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode) {
+    return res.redirect('/')
+  }
+
+  const prodId = req.params.productId;
+  Products.fetchProductById(prodId, product => {
+    if (!product) {
+      return res.redirect('/')
+    }
+    res.render("admin/edit-product", {
+      docTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  })
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  // console.log(prodId);
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Products(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Products.deleteById(prodId);
+  res.redirect('/admin/products')
+}
+
+exports.getProducts = (req, res, next) => {
+  Products.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      docTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};

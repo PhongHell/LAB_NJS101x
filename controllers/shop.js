dc9515cc46e26b480c54353422c39a183ee3aaa2
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 22:20:44 +0700

    connect mySQL for app.js

---
+++ controllers/shop.js
@@ -0,0 +1,74 @@
+const Products = require("../models/product");
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Products.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      docTitle: "All products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Products.fetchProductById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      docTitle: product.title,
+      path: '/products'
+    })
+  })
+}
+
+exports.getIndex = (req, res, next) => {
+  Products.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      docTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+// Cart
+
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    Products.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProduct = cart.products.find(prod => prod.id === product.id);
+        if (cartProduct) {
+          cartProducts.push({ productData: product, qty: cartProduct.qty});
+        }
+      }
+      res.render('shop/cart', { path: '/cart', docTitle: "Your cart", products: cartProducts })
+    })
+  })
+}
+
+exports.postCart =  (req, res, next) => {
+  const prodId = req.body.productId;
+  Products.fetchProductById(prodId, product => {
+    Cart.addToCart(product.id, product.price);
+    res.redirect('/cart');
+  })
+}
+
+exports.deleteCartProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Products.fetchProductById(prodId, product => {
+    Cart.deleteById(prodId, product.price);
+    res.redirect('/cart');
+  })
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', { path: '/orders', docTitle: "Your orders" })
+}
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', { path: '/checkout', docTitle: "Checkout" })
+}

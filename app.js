38b32c01645deeee62ba45e85e2d88dbdbbd188f
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 13:26:31 +0700

    changr pug to handlebars

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const adminData = require('./routes/admin');
+const bodyParser = require('body-parser');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+app.use((req, res ,next) =>{
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404' , {pageTitle : "Page Not Found"});
+});
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router  = express.Router();
+const path = require('path');
+
+const rootDir  = require('../util/path');
+
+const adminData  = require('./admin');
+
+router.get('/', (req, res , next) => {
+   // res.sendFile(path.join(rootDir, 'views' , 'shop.html'));
+   // console.log('shop.js', adminData.products);
+   const products = adminData.products;
+   res.render('shop', {prods : products, pageTitle : 'Shop Page', path : '/', hasProduct :products.length > 0});
+});
+module.exports = router ;

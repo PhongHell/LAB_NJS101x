d5292096c94743e23ee266504a5c6adb0251d42c
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-09 13:43:02 +0700

    create controller

---
+++ controllers/products.js
@@ -0,0 +1,28 @@
+const products = [];
+
+exports.getAddProduct = (req, res , next) => {
+
+    res.render('add-product',{
+        pageTitle : 'add-product ', 
+        path : '/admin/add-product',
+        productCSS : true, 
+        formsCSS : true , 
+        activeAddProduct : true
+    });
+};
+
+exports.postAddProduct =  (req, res, next) =>{
+    products.push({title : req.body.title});
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res , next) => {//logic of shop.js
+    res.render('shop', {
+                         prods : products,
+                         pageTitle : 'Shop Page', 
+                         path : '/', 
+                         hasProduct :products.length > 0 , 
+                         activeShop : true, 
+                         productCSS : true
+                      });
+             }

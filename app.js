7b20d9d7894529f9a25e5a8f9a6fc316ac7a2976
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-10 15:20:02 +0700

    create schema product /model

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require('mongoose');
+
+const path = require("path");
+// const rootDir = require('./util/path');
+
+// Routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// user model
+const User = require('./models/user');
+
+// Controllers
+const errorsController = require("./controllers/errors");
+
+// Create app
+const app = express();
+
+app.set("view engine", "ejs");
+
+// Store user in request
+app.use((req, res, next) => {
+  User.findById('6186750a4b187bac5e7c3f14').then(user => {
+    req.user = new User(user.name, user.email, user.cart, user._id);
+    next();
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use("/", errorsController.get404);
+
+mongoose.connect('mongodb+srv://Kb3X1knoBJT4xRUR:Kb3X1knoBJT4xRUR@cluster0.6lc11.mongodb.net/shop?retryWrites=true&w=majoritymongodb+srv://ThanhPhong:23lOQy6qMnphokd2@cluster0.n29hy.mongodb.net/shop?retryWrites=true&w=majority')
+.then((results) => {
+  app.listen(3000)
+})
+.catch(err => {
+  console.log(err)
+})
---
+++ routes/shop.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// const path = require('path');
+
+// const rootDir = require('../util/path');
+// const adminData = require("./admin");
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+//Products
+
+// router.get("/", shopController.getIndex);
+
+// router.get('/products', shopController.getProducts);
+
+// router.get('/product/:productId', shopController.getProduct);
+
+// // //Cart
+
+// router.get('/cart', shopController.getCart);
+
+// router.post('/cart', shopController.postCart);
+
+// router.post('/delete-cart-product', shopController.deleteCartProduct);
+
+// // // Order
+// router.post('/orders', shopController.postOrder);
+
+// router.get('/orders', shopController.getOrders);
+
+
+
+// router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;

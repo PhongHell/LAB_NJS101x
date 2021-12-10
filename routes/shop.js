bbd1ab4b3414416c23ce948584aa42568ee1997a
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-10 23:02:31 +0700

    using middleware protection router

---
+++ routes/shop.js
@@ -0,0 +1,37 @@
+const express = require("express");
+// const path = require('path');
+
+// const rootDir = require('../util/path');
+// const adminData = require("./admin");
+
+const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+//Products
+
+router.get("/", shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/product/:productId', shopController.getProduct);
+
+// // //Cart
+
+router.get('/cart', isAuth, shopController.getCart);
+
+router.post('/cart', isAuth, shopController.postCart);
+
+router.post('/delete-cart-product', isAuth, shopController.deleteCartProduct);
+
+// Order
+router.post('/orders', isAuth, shopController.postOrder);
+
+router.get('/orders', isAuth, shopController.getOrders);
+
+
+
+// router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;

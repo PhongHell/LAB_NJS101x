c5ad8592ac47e45d9bfd31a7dc2428aa670e91d3
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-11 00:10:51 +0700

    more validators

---
+++ routes/auth.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const authController = require("../controllers/auth");
+
+const router = express.Router();
+
+const { check, body } = require("express-validator");
+
+router.get("/login", authController.getLogin);
+
+router.get(
+  "/signup",
+  authController.getSignup
+);
+
+router.post(
+  "/login",
+  check("email").isEmail().withMessage("Please enter a valid email."),
+  authController.postLogin
+);
+
+router.post("/signup",
+[
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email is forbidden");
+      }
+      return true;
+    }),
+  body('password', 'Please enter password at least 6 characters with alphabet text and number.')
+  .isLength({min: 6})
+  .isAlphanumeric(),
+],
+authController.postSignup);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;

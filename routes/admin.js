a61750264f7fdfc71da3be5ac483415c4b5c0c76
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-10 15:18:37 +0700

    connect with mongose/app.js

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const path = require('path');
+// const rootDir = require('../util/path');
+
+const adminController = require('../controllers/admin')
+
+const router = express.Router();
+
+// router.get('/products', adminController.getProducts);
+
+// router.get('/add-product', adminController.getAddProduct);
+
+// router.post('/add-product', adminController.postAddProduct);
+
+// router.post('/edit-product', adminController.postEditProduct);
+
+// router.get('/edit-product/:productId', adminController.getEditProduct);
+
+// router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;

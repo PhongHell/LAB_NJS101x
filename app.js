837e1431943bd3372a239dd2506f55ba11c64f6d
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-12-10 15:34:04 +0700

    create and getting orders

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require('mongoose');
+
+const path = require("path");
+// const rootDir = require('./util/path');
+
+// Routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// user model
+const User = require('./models/user');
+
+// Controllers
+const errorsController = require("./controllers/errors");
+
+// Create app
+const app = express();
+
+app.set("view engine", "ejs");
+
+// Store user in request
+app.use((req, res, next) => {
+  User.findById('61a04f133f2eb5c7fe7312ce').then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use("/", errorsController.get404);
+
+mongoose.connect('mongodb+srv://ThanhPhong:23lOQy6qMnphokd2@cluster0.n29hy.mongodb.net/shop?retryWrites=true&w=majority')
+.then((results) => {
+  app.listen(3000);
+  User.findOne()    // findOne with no argument give back the first one
+  .then(user => {
+    if(!user) {
+      const user = new User({
+        name: 'Phong',
+        email: 'lala@gmail.com',
+        cart: []
+      });
+      user.save()
+    }
+  })
+})
+.catch(err => {
+  console.log(err)
+})
